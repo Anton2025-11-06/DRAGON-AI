6929a238453f7226ff7bbd2d16046914582c96e8
Anton <Anton@users.noreply.github.com>
2026-09-04 21:20:33 +0800

    nacos 配置改为本地环境（127.0.0.1，命名空间 dragon-ai）

--- common/common_nacos/config.py
+++ common/common_nacos/config.py
@@ -5,7 +5,7 @@ import logging
 @dataclasses.dataclass
 class Config:
     nacos_name = "nacos"
-    nacos_password = "Inforeiot2025."
-    nacos_server_address = "10.88.129.3:8848"
-    nacos_namespace_id = "bigdata-python-service-test"
+    nacos_password = "nacos"
+    nacos_server_address = "127.0.0.1:8848"
+    nacos_namespace_id = "dragon-ai"
     nacos_log_level = logging.INFO
